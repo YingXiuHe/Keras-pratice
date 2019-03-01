3a86fb156733a088f64ed6e6f85041cd9c5f8ccd
YingXiuHe <1105834881@qq.com>
2019-03-01 14:39:29 +0800

    Add files via upload

---
+++ AlexNet.py
@@ -0,0 +1,33 @@
+#-*-coding: utf-8-*-
+
+import keras 
+from keras.layers import Flatten, Conv2D, MaxPool2D, Dropout, Activation, Dense
+from keras.models import Sequential, Model
+from keras.utils import plot_model
+from IPython.display import Image
+
+input_shape = (227,227,3)
+model = Sequential(name='AlexNet')
+
+model.add(Conv2D(96, (11,11), strides=(4,4), activation='relu',padding='valid', input_shape=input_shape, kernel_initializer='uniform'))
+model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 3)))
+
+model.add(Conv2D(256, (5, 5), strides=(1, 1),activation='relu', padding='same', kernel_initializer='uniform'))
+model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+
+model.add(Conv2D(384, (3,3), strides=(1,1), activation='relu', padding='same', kernel_initializer='uniform'))
+model.add(Conv2D(384, (3,3), strides=(1,1), activation='relu', padding='same', kernel_initializer='uniform'))
+model.add(Conv2D(256, (3,3), strides=(1,1), activation='relu', padding='same', kernel_initializer='uniform'))
+model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+
+model.add(Flatten(name='Flatten_layer'))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1000, activation='softmax', name='predicts'))
+
+model.summary()
+
+plot_model(model, to_file='AlexNet.png')
+Image('AlexNet.png')
